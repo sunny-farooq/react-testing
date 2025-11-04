1d7a74cca616601e41955cbbac0ef1c1fe3b522e
Sunny Farooq <msaneefarooq34@yahoo.com>
2025-11-04 18:38:06 +0500

    testing previous

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/SignUp";
 import Login from "./pages/Login";
 import "./App.css"
-import Chat from "./pages/chatpage";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
+      
     </Routes>
   );
 }
